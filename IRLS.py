80f4be65cfb7292dd1f91b7e5d7343327c7a649e
Zane Jakobs <zane.jakobs@colorado.edu>
2019-04-04 08:18:24 -0600

    Added LP regression with IRLS fitting, plotting methods

--- IRLS.py
+++ IRLS.py
@@ -20,12 +20,17 @@ class IRLS:
     #Author: Zane Jakobs
     # param external_data: predictor data input as matrix
     # param yobs: observed values of regressor (variable to predict)
-    def __init__(self, external_data, yobs):
+    # param objective: what are we solving for? Supported options 
+    # param norm: norm to use in solving for resids
+    # include only "L1" right now
+    def __init__(self, external_data, yobs,objective = "L1",norm = 1 ):
         if np.ma.size(external_data, 0) != np.ma.size(yobs,0):
             print("Error: lengths are not the same.")
         else:
             self.external_data = external_data
             self.yobs = yobs
+            self.objective = objective
+            self.p = norm
             
     def update_predictors(self, new_data):
         self.external_data = new_data
@@ -33,19 +38,29 @@ class IRLS:
     def update_observations(self, new_yobs):
         self.yobs = new_yobs
         
+        
+    @jit
+    def regularize_resids(self,resids):
+        for e in range(np.ma.size(resids,0)):
+                if resids[e] == 0:
+                    resids[e] = 1.0e-4
+        return resids
     # Author: Zane Jakobs
     # param b: objective vector
     # param a: matrix
     # param x: current x value in the matrix's preimage
     # return: b - ax; the L1 error
     @jit
-    def resids(self,y,a,beta):
-        resids = (y-np.matmul(a,beta))
-        for e in range(np.ma.size(resids,0)):
-                if resids[e] == 0:
-                    resids[e] = 1.0e-2
+    def L1Resids(self,y,a,beta):
+        resids = y-np.matmul(a,beta)
+        resids = self.regularize_resids(resids)
         return resids
-                    
+        
+    @jit
+    def LpResids(self,y,a,beta):
+        resids = y-np.matmul(a,beta)
+        resids = self.regularize_resids(np.power(resids,self.p))
+        return resids
         
 
     # Author: Zane Jakobs
@@ -56,7 +71,13 @@ class IRLS:
         tot = 0
         for r in np.nditer(resids):
             tot = tot + abs(r)
-            return tot
+        return tot
+    @jit
+    def LP_loss_func(self,resids):
+        tot = 0
+        for r in np.nditer(resids):
+            tot = tot + abs(r)**self.p
+        return tot
 
     # Author: Zane Jakobs
     # param resids: vector residuals
@@ -72,8 +93,8 @@ class IRLS:
     # param Einv: result of a call to resid_inv_mat
     # return: GLS model matrix with covariance matrix Einv (A^T EInv A)
     @jit
-    def model_matrix(self,a,Einv):
-        return np.matmul(np.transpose(a), Einv*a )
+    def model_matrix(self,a,covmat):
+        return np.matmul(np.transpose(a), np.matmul(covmat,a) )
 
 
     # Author: Zane Jakobs
@@ -98,6 +119,26 @@ class IRLS:
             tot = tot + (o - n)**2
         return tot
     
+    # Author: Zane Jakobs
+    # param obs: observations
+    # param pred: predictors
+    # param beta: estiamte of beta parameters
+    def L1SolutionLoop(self,obs,pred,beta):
+        epsilon = self.L1Resids(obs,pred,beta)
+        Einv = self.resid_inv_mat(epsilon)
+        modelMat = self.model_matrix(pred,Einv)
+        newBeta = self.update_vector(obs, pred, Einv, modelMat)
+        rdict = {1:newBeta,2:self.loss_func(epsilon)}
+        return rdict
+        
+    def LPSolutionLoop(self, obs, pred, beta):
+        epsilon = self.LpResids(obs,pred,beta)
+        weights = self.resid_inv_mat(epsilon)
+        modelMat = self.model_matrix(pred,weights)
+        newBeta = self.update_vector(obs,pred,weights,modelMat)
+        rdict = {1:newBeta,2:self.LP_loss_func(epsilon), 3:np.diag(weights)}
+        return rdict
+        
     # Author: Zane Jakobs
     # return: optimal solution
     @jit
@@ -126,16 +167,29 @@ class IRLS:
         errList = []
         #keep updating beta until convergence
         while newOldDist > tolerance and num_iter < max_iter:
-            epsilon = self.resids(obs,predictors,beta)
-            Einv = self.resid_inv_mat(epsilon)
-            modelMat = self.model_matrix(predictors,Einv)
-            newBeta = self.update_vector(obs, predictors, Einv, modelMat)
-
-            newOldDist = self.sum_square_difference(beta, newBeta)
-            betaList.append(beta)
-            errList.append(self.loss_func(epsilon))
-            beta = newBeta
-            num_iter = num_iter + 1
+            if self.objective == "L1":
+                newData = self.L1SolutionLoop(obs, predictors, beta)
+                newBeta = newData[1]
+                errList.append(newData[2])
+                betaList.append(beta)
+                newOldDist = self.sum_square_difference(beta, newBeta)
+                beta = newBeta
+                num_iter = num_iter + 1
+                
+            elif self.objective == "LP":
+                newData = self.LPSolutionLoop(obs,predictors, beta)
+                newBeta = newData[1]
+                errList.append(newData[2])
+                betaList.append(beta)
+                newOldDist = self.sum_square_difference(beta, newBeta)
+                beta = newBeta
+                num_iter = num_iter + 1
+                
+            else:
+                print("Sorry, no  objective functions other than L1 and LP are currently supported")
+                break
+            
+            
         if num_iter >= max_iter - 1:
             print('Solver failed to converge in ',num_iter ,' iterations')
         else:
@@ -148,11 +202,21 @@ class IRLS:
     #Author: Zane Jakobs
     #description: plots loss versus iteration
     def plot_loss_vs_iter(self, solution_dict):
+        plt.figure()
         plt.plot(solution_dict["Errors"],'r--')
         plt.ylabel("Loss function")
         plt.xlabel("Iteration")
         plt.show()
     
+    def plot_uni_regression_line(self, solution_dict):
+        beta = solution_dict["Coefficients"]
+        plt.figure()
+        plt.plot(self.external_data[:,1],self.yobs,'bx')
+        plt.xlabel("Predictor")
+        plt.ylabel("Data")
+        plt.plot(self.external_data[:,1], self.external_data*beta,'-r')
+        plt.show()
+        
     
     
     
