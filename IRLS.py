8c4da16469eb9587cffaf213ab0a41b47a72eff4
Zane Jakobs <zane.jakobs@colorado.edu>
2019-04-04 15:04:48 -0600

    Fixed errors with JIT and larger datasets

--- IRLS.py
+++ IRLS.py
@@ -11,7 +11,6 @@ using iteratively reweighted least squares
 from numba import jit
 import numpy as np
 import matplotlib.pyplot as plt
-
 # Author: Zane Jakobs
 # description: a class to perform L1 regression
 # with iteratively reweighted least square
@@ -39,7 +38,7 @@ class IRLS:
         self.yobs = new_yobs
         
         
-    @jit
+    #@jit
     def regularize_resids(self,resids):
         for e in range(np.ma.size(resids,0)):
                 if resids[e] == 0:
@@ -50,13 +49,13 @@ class IRLS:
     # param a: matrix
     # param x: current x value in the matrix's preimage
     # return: b - ax; the L1 error
-    @jit
+    #@jit
     def L1Resids(self,y,a,beta):
         resids = y-np.matmul(a,beta)
         resids = self.regularize_resids(resids)
         return resids
         
-    @jit
+    #@jit
     def LpResids(self,y,a,beta):
         resids = y-np.matmul(a,beta)
         resids = self.regularize_resids(np.power(resids,self.p))
@@ -66,13 +65,13 @@ class IRLS:
     # Author: Zane Jakobs
     # param resids: residuals vector
     # return: sum of abs values of resids 
-    @jit
+    #@jit
     def loss_func(self,resids):
         tot = 0
         for r in np.nditer(resids):
             tot = tot + abs(r)
         return tot
-    @jit
+    #@jit
     def LP_loss_func(self,resids):
         tot = 0
         for r in np.nditer(resids):
@@ -82,7 +81,7 @@ class IRLS:
     # Author: Zane Jakobs
     # param resids: vector residuals
     # return matrix whose diagonal elements E[i,i] = 1/resids[i]
-    @jit
+    #@jit
     def resid_inv_mat(self, resids):
         #assert none of the residuals are 0
         assert np.all(resids != 0)
@@ -92,7 +91,7 @@ class IRLS:
     # param a: constraint/data matrix
     # param Einv: result of a call to resid_inv_mat
     # return: GLS model matrix with covariance matrix Einv (A^T EInv A)
-    @jit
+    #@jit
     def model_matrix(self,a,covmat):
         return np.matmul(np.transpose(a), np.matmul(covmat,a) )
 
@@ -103,7 +102,7 @@ class IRLS:
     # param a: matrix
     # param currX: current x value in the matrix's preimage
     # return newX: new best solution
-    @jit
+    #@jit
     def update_vector(self,y,obvs, Einv, modelMat):
         rhs = np.matmul(np.transpose(obvs), np.matmul(Einv,y))
         return np.linalg.solve(modelMat,rhs)
@@ -113,41 +112,64 @@ class IRLS:
     #param new: x^t
     #return: sum of squares of differences--that is,
     # sum([new-old]^2))
-    def sum_square_difference(self,old,new):
+    #@jit
+    def sum_square_difference(self,old,new,p,scale):
         tot = 0.0
-        for o,n in zip(old,new):
+        for i in range(p):
+            o = old[i]
+            n = new[i]
             tot = tot + (o - n)**2
-        return tot
+            
+        return tot/scale
     
     # Author: Zane Jakobs
     # param obs: observations
     # param pred: predictors
     # param beta: estiamte of beta parameters
+    #@jit
     def L1SolutionLoop(self,obs,pred,beta):
         epsilon = self.L1Resids(obs,pred,beta)
-        Einv = self.resid_inv_mat(epsilon)
+        Einv = 0.5*self.resid_inv_mat(epsilon)
         modelMat = self.model_matrix(pred,Einv)
         newBeta = self.update_vector(obs, pred, Einv, modelMat)
         rdict = {1:newBeta,2:self.loss_func(epsilon)}
         return rdict
         
+   # @jit
     def LPSolutionLoop(self, obs, pred, beta):
         epsilon = self.LpResids(obs,pred,beta)
-        weights = self.resid_inv_mat(epsilon)
+        if self.p == 2:
+            weights = self.resid_inv_mat(epsilon)
+        else:
+            weights = self.p *0.5* (self.resid_inv_mat(epsilon))**( self.p - 2 )
         modelMat = self.model_matrix(pred,weights)
         newBeta = self.update_vector(obs,pred,weights,modelMat)
         rdict = {1:newBeta,2:self.LP_loss_func(epsilon), 3:np.diag(weights)}
         return rdict
-        
+
+    #@jit
+    def getBestBeta(self,betaList, errList):
+        minErr = 1.0e15
+        listLen = np.ma.size(errList)
+        for n in range(listLen):
+            if errList[n] < minErr and errList[n] != 0:
+                best = n
+        return betaList[best]
+    
     # Author: Zane Jakobs
     # return: optimal solution
-    @jit
+    #@jit
     def solve(self, tolerance = 1.0e-12):
-        #print("solve")
+        print("Fitting ", self.objective, " regression.")
         #predictors
         predictors = self.external_data
         #observations
         obs = self.yobs
+        n = np.ma.size(obs,0)
+        
+        obs = obs.reshape((n,1))
+        #print(predictors.shape)
+        #print(obs.shape)
         #number of predictors
         p = np.ma.size(predictors, 1)
         #number of datapoints
@@ -163,8 +185,9 @@ class IRLS:
         num_iter = 0
         converged = False
         #arrays to hold stats by iteration
-        betaList = []
-        errList = []
+        betaList = [beta]
+        errList = [0.0]
+        betaScale = 1#obs[4]
         #keep updating beta until convergence
         while newOldDist > tolerance and num_iter < max_iter:
             if self.objective == "L1":
@@ -172,7 +195,10 @@ class IRLS:
                 newBeta = newData[1]
                 errList.append(newData[2])
                 betaList.append(beta)
-                newOldDist = self.sum_square_difference(beta, newBeta)
+                if num_iter == 1:
+                    newOldDist = self.sum_square_difference(beta, newBeta,p,betaScale)
+                else:
+                    newOldDist = self.sum_square_difference(beta, newBeta,p,betaScale)
                 beta = newBeta
                 num_iter = num_iter + 1
                 
@@ -183,10 +209,11 @@ class IRLS:
                 newBeta = newData[1]
                 errList.append(newData[2])
                 betaList.append(beta)
-                newOldDist = self.sum_square_difference(beta, newBeta)
+                newOldDist = self.sum_square_difference(beta, newBeta,p,betaScale)
                 beta = newBeta
                 num_iter = num_iter + 1
                 
+            
             else:
                 print("Sorry, no  objective functions other than L1 and LP are currently supported")
                 break
@@ -194,9 +221,12 @@ class IRLS:
             
         if num_iter >= max_iter - 1:
             print('Solver failed to converge in ',num_iter ,' iterations')
+            beta = self.getBestBeta(betaList, errList[1:])
+            
         else:
             converged = True
-        solution_dict = {'Coefficients':beta, 'Errors':errList, 'Beta_v_Iter' : betaList,
+            
+        solution_dict = {'Coefficients':beta, 'Errors':errList[1:], 'Beta_v_Iter' : betaList,
                          'Convergence': converged, 
                          'NumIterations':num_iter}
         return solution_dict
@@ -212,11 +242,12 @@ class IRLS:
     
     def plot_uni_regression_line(self, solution_dict):
         beta = solution_dict["Coefficients"]
+        line = np.matmul( self.external_data,beta)
         plt.figure()
-        plt.plot(self.external_data[:,1],self.yobs,'bx')
+        plt.plot(range(np.ma.size(self.yobs)), self.yobs,'bx')
         plt.xlabel("Predictor")
         plt.ylabel("Data")
-        plt.plot(self.external_data[:,1], self.external_data*beta,'-r')
+        plt.plot(range(np.ma.size(self.yobs)), line,'-r')
         plt.show()
         
     
